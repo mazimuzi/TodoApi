47a21138ab182ea0a4fcfd807172c8d3da1c1560
mazimuzi <eemelisalmikunnas@gmail.com>
2024-09-19 18:28:44 +0300

    deez

--- Program.cs
+++ Program.cs
@@ -6,13 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("TodoDB")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    TodoContext dbcontext = scope.ServiceProvider.GetRequiredService<TodoContext>();
+    dbcontext.Database.EnsureCreated();
+
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
